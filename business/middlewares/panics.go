5c2c3e3fd3a877d58a7653591046b9eed82b203a
Daniel <dani.mbirochi@gmail.com>
2021-10-20 11:44:57 -0300

    Adds middl for handling panics propagated by inner handlers in the chain

---
+++ business/middlewares/panics.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/danielmbirochi/go-sample-service/foundation/web"
+	"github.com/pkg/errors"
+)
+
+// Panics recovers from panics propagated by the innerHandler and converts the panic
+// to an error so it is handled in Errors middleware (outer middleware).
+func Panics(log *log.Logger) web.Middleware {
+
+	m := func(innerHandler web.Handler) web.Handler {
+
+		// Create the Panic handler that will be attached in the middleware onion chain.
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) (err error) {
+
+			v, ok := ctx.Value(web.KeyValues).(*web.Values)
+			if !ok {
+				return web.NewShutdownError("web value missing from context")
+			}
+
+			// Defer a function to recover from a panic and set the err return
+			// variable after the fact.
+			defer func() {
+				if r := recover(); r != nil {
+					err = errors.Errorf("panic: %v", r)
+
+					// Log the Go stack trace for this panic'd goroutine.
+					log.Printf("%s :\n%s", v.TraceID, debug.Stack())
+				}
+			}()
+
+			// Call the next Handler and set its return value in the err variable of this context.
+			return innerHandler(ctx, w, r)
+		}
+
+		return h
+	}
+
+	return m
+}
