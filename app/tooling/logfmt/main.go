cdf92e83d2362a554e46d61bf621a5d644481805
Daniel <dani.mbirochi@gmail.com>
2021-11-08 10:01:17 -0300

    Adds a simple program to format structured logs in a human readable way

---
+++ app/tooling/logfmt/main.go
@@ -0,0 +1,77 @@
+// This program takes the structured log output and makes it human readable.
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var service string
+
+func init() {
+	flag.StringVar(&service, "service", "", "filter which service to see")
+}
+
+func main() {
+	flag.Parse()
+	var b strings.Builder
+
+	// Scan standard input for log data per line.
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		// Convert the JSON log to a map for processing it.
+		m := make(map[string]interface{})
+		err := json.Unmarshal([]byte(s), &m)
+		if err != nil {
+			if service == "" {
+				fmt.Println(s)
+			}
+			continue
+		}
+
+		// Set default tractId
+		traceID := "00000000-0000-0000-0000-000000000000"
+		if v, ok := m["traceid"]; ok {
+			traceID = fmt.Sprintf("%v", v)
+		}
+
+		// Order the portions of the zap logger output.
+		b.Reset()
+		b.WriteString(fmt.Sprintf("%s: %s: %s: %s: %s: %s: ",
+			m["service"],
+			m["ts"],
+			m["level"],
+			traceID,
+			m["caller"],
+			m["msg"],
+		))
+
+		// Add the rest of the keys ignoring the ones we already
+		// added for the log.
+		for k, v := range m {
+			switch k {
+			case "service", "ts", "level", "traceid", "caller", "msg":
+				continue
+			}
+
+			// It's nice to see the key[value] in this format
+			// especially since map ordering is random.
+			b.WriteString(fmt.Sprintf("%s[%v]: ", k, v))
+		}
+
+		// Removes the last : from the new log format
+		out := b.String()
+		fmt.Println(out[:len(out)-2])
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
